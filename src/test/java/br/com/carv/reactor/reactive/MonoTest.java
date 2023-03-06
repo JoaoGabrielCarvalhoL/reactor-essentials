82809fff27317acc2ea4b72d45c9064cc9480bb8
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2023-03-06 18:27:25 -0300

    Start reactor project

---
+++ src/test/java/br/com/carv/reactor/reactive/MonoTest.java
@@ -0,0 +1,15 @@
+package br.com.carv.reactor.reactive;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.logging.Logger;
+
+public class MonoTest {
+
+    private Logger logger = Logger.getLogger(MonoTest.class.getSimpleName());
+
+    @Test
+    public void test() {
+        logger.info("Everything work as intended!");
+    }
+}
